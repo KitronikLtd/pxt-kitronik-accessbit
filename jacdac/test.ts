f3d74a5c6aca63cdc30550d7d4757f3b8758e134
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-28 10:09:55 -0700

    better servo support

--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,1 +1,8 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let a = 90
+forever(() => {
+    modules.servo1.setEnabled(true)
+    modules.servo1.setAngle(a + 90)
+    console.logValue("angle", a + 90)
+    a = -a
+    pause(2000)
+})
